27049df64c963694531625d3c23b2ef6770eb20c
Aakash1006 <aakashbapuchavan@gmail.com>
2020-06-03 01:31:54 +0530

    Predict using command line

--- colorpredict.py
+++ colorpredict.py
@@ -2,9 +2,15 @@ import numpy as np
 import cv2
 from keras.preprocessing import image
 import tensorflow as tf
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+	help="path to input image")
+args = vars(ap.parse_args())
 
 labels=["Red","Green","Blue"]
-test_image=cv2.imread("318.png")
+test_image=cv2.imread(args["input"])
 image=cv2.resize(test_image,(28,28))
 image=image.astype("float32")/255.0
 image = np.expand_dims(image, axis=0)
